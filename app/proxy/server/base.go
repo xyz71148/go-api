96197298700adac5972f8e1a921ce06e1ead96e9
dhole <dhole.me@gmail.com>
2020-05-22 13:11:05 +0800

    deploy

--- app/proxy/server/base.go
+++ app/proxy/server/base.go
@@ -1,7 +1,6 @@
 package server
 
 import (
-	"fmt"
 	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 	"log"
@@ -23,6 +22,6 @@ func (server *Server) Run(addr string) {
 	log.Fatal(http.ListenAndServe(addr, handlers.CORS(
 		handlers.AllowedHeaders([]string{"X-Requested-With", "Content-Type", "Authorization"}),
 		handlers.AllowedMethods([]string{"GET", "POST", "PUT", "HEAD", "DELETE", "OPTIONS"}),
-		handlers.AllowedOrigins([]string{fmt.Sprintf("http://%s",addr)}),
+		handlers.AllowedOrigins([]string{"*"}),
 		)(server.Router)))
 }
